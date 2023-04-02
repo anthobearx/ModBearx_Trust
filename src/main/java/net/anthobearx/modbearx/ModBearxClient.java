c07781a3b8cb0e7bcad6a223b61f03a098cce453
anthobearx <anthobearmc@gmail.com>
2023-04-02 15:16:48 -0700

    Primer client ejecutable

---
+++ src/main/java/net/anthobearx/modbearx/ModBearxClient.java
@@ -0,0 +1,10 @@
+package net.anthobearx.modbearx;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModBearxClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
--- src/main/java/net/anthobearx/modbearx/mixin/ModBearxMixin.java
+++ src/main/java/net/anthobearx/modbearx/mixin/ModBearxMixin.java
@@ -1,6 +1,6 @@
-package net.anthobearx.modbearxtrust.mixin;
+package net.anthobearx.modbearx.mixin;
 
-import net.anthobearx.modbearxtrust.ExampleMod;
+import net.anthobearx.modbearx.ModBearx;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,9 +8,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class ModBearxMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		ModBearx.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
